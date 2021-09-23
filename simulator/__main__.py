3dafa2591f7cc75eac7f471cdbc01c92379cecd5
Paulo Severo <opaulosevero@gmail.com>
2021-09-23 19:19:13 -0300

    [New] Basic directory structure

---
+++ simulator/__main__.py
@@ -0,0 +1,11 @@
+# pylint: disable=invalid-name
+"""Contains the basic structure necessary to execute the simulation.
+"""
+
+
+def main():
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ simulator/maintenance.py
@@ -0,0 +1,3 @@
+"""Contains helper methods that extend EdgeSimPy to simulate maintenance in edge infrastructures.
+"""
+...
